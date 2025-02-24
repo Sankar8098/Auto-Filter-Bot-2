df5184115f010adb79477a7153700d784596e35b
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2025-02-24 13:51:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -131,7 +131,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "cute-adelheid-thankyousir-0a9db8d7.koyeb.app")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
